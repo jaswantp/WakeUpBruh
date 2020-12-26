bf962c9b58ab21f11ad466df50a150ad560be5e5
Jaswant Panchumarti <jaswantphysx@gmail.com>
2020-12-26 13:51:35 +0530

    Demo - use lambda

--- wub.cpp
+++ wub.cpp
@@ -21,8 +21,12 @@ main(int argc, char* argv[])
   SomeObject obj;
   int id = 0;
   Timer timer;
-  auto tid0 = timer.add(6000, true, func, obj, 0);
-  auto tid1 = timer.add(4000, false, func, obj, 1);
+  auto tid0 = timer.add(6000, true, func, obj, 0); // oneshot
+  auto tid1 = timer.add(4000, false, func, obj, 1); // repeating
+  auto tid2 = timer.add(10000, true, [&](int _id) {
+    std::cout << "\n Timer: " << 2 << "| Wake The Fuck Up Bruhh ._."
+              << " " << obj.a << obj.b << "\n";
+  }, 2); // oneshot w/ lambda? Heck yea.
   {
     using namespace std;
     std::this_thread::sleep_for(8000ms);
